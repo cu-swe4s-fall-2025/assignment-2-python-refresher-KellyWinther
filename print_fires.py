3d4a13397296148e7cd8e2fb6acceb070585b244
KellyWinther <kelly.winther@colorado.edu>
2025-09-08 10:29:52 -0600

    Updated this code to reorder the parameters to match the order needed for the get_column function, and used the get_column function to find fires in Jordan.

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,15 @@
-country='United States of America'
-county_column = 1
-fires_column = 4
+
+import my_utils
+
+#Identify file you want to read:
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
-print(fires)
+
+#Identify search parameters:
+county_column = 0 #query_column = column index for country name
+country='Jordan' #query_value = country of interest
+fires_column = 3 #result_column = column indext for forest fires
+
+#Run the function get_column to get the number of forest fires in the specified country:
+fires = my_utils.get_column(file_name, county_column, country, fires_column)
+
+print(f'Across different years of reports, there were {fires} forest fires in {country}.')
