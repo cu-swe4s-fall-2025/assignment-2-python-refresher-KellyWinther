3b9ee32beedefe43f932bd569d44c8d9b49c3da6
KellyWinther <kelly.winther@colorado.edu>
2025-09-22 23:16:29 -0600

    Assignment 3 best practices homework updates to assignment 2

--- run.sh
+++ run.sh
@@ -3,7 +3,30 @@
 # This script runs a Python program that reports the number of fires in the specified country.
 # It uses the 'my_utils.py' function 'get_column' to read data from a CSV file.
 
-python3 print_fires.py
+# make sure to give execute permissions with: chmod +x run.sh
+# then run with: ./run.sh
 
-#make sure to give execute permissions with: chmod +x run.sh
-#then run with: ./run.sh
+# Example 1 - successful run
+echo "-- Example of a successful run --"
+python3 print_fires.py \
+    --country "United States of America" \
+    --country_column 0 \
+    --fires_column 2 \
+    --file_name "Agrofood_co2_emission.csv"
+
+
+# Example 2 - unsuccessful run (missing country_column)
+echo "-- Example of an unsuccessful run (missing country_column) --"
+python3 print_fires.py \
+    --country "United States of America" \
+    --fires_column 2 \
+    --file_name "Agrofood_co2_emission.csv"
+
+
+# Example 3 - unsuccessful run (file not found)
+echo "-- Example of an unsuccessful run (file not found) --"
+python3 print_fires.py \
+    --country "United States of America" \
+    --country_column 0 \
+    --fires_column 2 \
+    --file_name "I_do_not_exist.csv"
