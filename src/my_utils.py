f5b4804a48ea48fcebe211f55a7e23098722529a
KellyWinther <kelly.winther@colorado.edu>
2025-10-07 13:31:56 -0600

    fixed style errors in my_utils.py

--- src/my_utils.py
+++ src/my_utils.py
@@ -4,45 +4,45 @@ import sys
 def get_column(file_name, query_column, query_value, result_column=1):
 
     """
-    This function reads a CSV file and retrieves values from a specified column.
+    Read a CSV file and retrieve values from a specified column.
 
     Parameters:
     - file_name (str): Name of the CSV file to read.
-    - query_column (int): Index of the column that you want to search for specific values.
+    - query_column (int): Index of column to search within.
     - query_value (str): Value to search for in the specified query_column.
-    - result_column (int): Index of the column you want to retrieve new values. Default is 1.
+    - result_column (int): Index of column to retrieve values. Default: 1
 
     Returns:
-    - List of integers: Values from result_column where the query_column == query_value.
+    - List (int): result_column values when query_column == query_value.
     """
 
     # Initialize an empty array to store results
     results = []
 
     # Open the file and read line by line:
-    try: 
+    try:
         with open(file_name, "r") as file:
             for line in file:
                 values = line.strip().split(",")
-                
-                # check if the value in the query_column matches the query_value
+
+                # does the query_column matches the query_value?
                 if values[query_column] == (query_value):
                     try:
-                        # convert to integers and append to results array if its a match
+                        # convert to integer and append to results array
                         results.append(int(float(values[result_column])))
 
                     except ValueError:
-                        print(f"Cannot convert value '{values[result_column]}'")
+                        print(f"Cannot convert: '{values[result_column]}'")
                         sys.exit(1)
 
     except FileNotFoundError:
         print(f"Error: File not found: {file_name}")
         sys.exit(1)
-    
+
     except IndexError:
         print("Error: query_column or result_column out of bounds")
         sys.exit(1)
-        
+
     return results
 
 
@@ -57,12 +57,12 @@ def mean(int_array):
     - mean_value (float): Mean value of the array.
     '''
 
-    try: 
+    try:
         mean_value = sum(int_array) / len(int_array)
 
     except ZeroDivisionError:
         print("Error: Cannot calculate mean of an empty array")
-        sys.exit(1)  
+        sys.exit(1)
 
     return mean_value
 
@@ -74,7 +74,7 @@ def median(int_array):
     Parameters:
     - int_array (list):list of numbers (does not need to be pre-sorted).
 
-    Returns: 
+    Returns:
     - median_value (float): The median value.
     '''
 
@@ -108,13 +108,13 @@ def stdev(array):
     try:
         array_mean = mean(array)
         residual_sum = 0
-    
+
         for value in array:
             residual_sum += (value - array_mean) ** 2
         std = (residual_sum / len(array)) ** 0.5
-    
+
     except ZeroDivisionError:
         print("Error: Cannot calculate standard deviation of an empty array")
         sys.exit(1)
-    
+
     return std
